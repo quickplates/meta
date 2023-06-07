5d91e05e20cc44150be50f38f3278a8f624be41d
Sebastian Pietras <me@spietras.dev>
2023-06-07 12:52:52 +0000

    Added more comments

--- .trunk/configs/.eslintrc.cjs
+++ .trunk/configs/.eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   parserOptions: {
+    // Use the latest version of ECMAScript
     ecmaVersion: "latest",
   },
+
+  // Don't ignore config files
   ignorePatterns: ["!.*"],
 };
